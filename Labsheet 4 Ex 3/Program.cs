c63f49ec8a21293e044c355920c9e6e3b7bc2a7e
s00224934 <s00224934@LAB-D2033-21>
2022-10-14 11:39:20 +0100

    Add project files.

---
+++ Labsheet 4 Ex 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_4_Ex_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            BankAccount acc1 = new BankAccount("45682012", "Troy Shilton", 100);
+            BankAccount acc2 =new BankAccount("75587891", "Eoghan Kearns", 200);
+
+            Console.WriteLine(acc1.GetAccountDetails());
+            Console.WriteLine(acc2.GetAccountDetails());
+
+            Console.WriteLine("Adding €250 to account 1");
+            acc1.Deposit(1250);
+            Console.WriteLine(acc1.GetAccountDetails());
+
+            Console.WriteLine("Withdrawing €25 from account 2");
+            acc2.Withdraw(25);
+            Console.WriteLine(acc2.GetAccountDetails());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Labsheet 4 Ex 4/CashRegister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_4_Ex_4
+{
+    public class CashRegister
+    {
+        public int NumberOfItems { get; private set; }
+
+        public double Total { get; private set; }
+
+        public void AddItem(double itemPrice)
+        {
+            Total += itemPrice;
+            NumberOfItems++;
+        }
+
+
+
+
+    }
+}
