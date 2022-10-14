49be53f880c06cb6f37ec8553d0589c5d2176a61
Abdul Khan <S00224934@atu.ie>
2022-10-14 11:47:26 +0100

    finished question 5

---
+++ Labsheet 4 Ex 5/CashRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labsheet_4_Ex_5
+{
+    internal class CashRegister
+    {
+        public static double totalMoneyAllCrs { get; set; }
+        public static int totalItemsAllCrs { get; set; }
+
+        public int NumberofItems { get; private set; }
+        public double Total { get;private set; }
+
+        public void AddItem(double itemPrice)
+        {
+            Total += itemPrice;
+            NumberofItems++;
+
+            totalItemsAllCrs++;
+            totalMoneyAllCrs += itemPrice;
+        }
+    }
+}
